1379d79232f2dc4d4b8e8ddccf2613c780389a36
AMMV21 <hatsune2108@gmail.com>
2023-12-12 15:35:02 +0100

    filtre BD v0.1

--- BDTK/Liste_BD/JS/filtre_BD.js
+++ BDTK/Liste_BD/JS/filtre_BD.js
@@ -1,9 +1,80 @@
-function addSerieFilter()
+let serieForm = document.getElementById('serieForm');
+let writerForm= document.getElementById('writerForm');
+let verifSerieTab =[];
+let verifWriterTab=[];
+
+window.addEventListener('load',()=>{addSerieFilter(serieForm)});
+window.addEventListener('load',()=>{addWriterFilter(writerForm)});
+
+function addSerieFilter(div)
 {
+    removeAllChild(div);
+
+    for(let i = 0; i<30;i++)
+    {
+        createSerieFilter(div,i);
+    }
 
 }
 
-function addWriterFilter()
+function addWriterFilter(div)
 {
-    
+    removeAllChild(div);
+
+    for(let i = 0; i<30;i++)
+    {
+        createWriterFilter(div,i);
+    }
+}
+
+function createSerieFilter(div,indice)
+{
+        let newDiv=document.createElement('div');
+        newDiv.setAttribute('class','divSerieItem');
+        let checkItem = document.createElement('input');
+        let labelItem = document.createElement('label');
+        let valSerie = newTab[indice][1].idSerie;
+        let serieTitle = series.get(valSerie).nom;
+
+        //on verifie si la série est déjà présente
+
+        if(!verifSerieTab.includes(serieTitle))
+        {
+            verifSerieTab.push(serieTitle);
+            div.appendChild(newDiv);
+            checkItem.type = 'checkbox';
+            checkItem.id=serieTitle;
+            labelItem.htmlFor =serieTitle;
+            labelItem.textContent=serieTitle;
+            newDiv.appendChild(checkItem);
+            newDiv.appendChild(labelItem);
+        }
+
+        
+}
+
+function createWriterFilter(div,indice)
+{
+        let newDiv=document.createElement('div');
+        newDiv.setAttribute('class','divSerieItem');
+        let checkItem = document.createElement('input');
+        let labelItem = document.createElement('label');
+        let valWriter = newTab[indice][1].idAuteur;
+        let writerName = auteurs.get(valWriter).nom;
+
+        writerName =writerName.replaceAll(',',' ');
+        //on verifie si la série est déjà présente
+
+        if(!verifWriterTab.includes(writerName))
+        {
+            verifWriterTab.push(writerName);
+            div.appendChild(newDiv);
+            checkItem.type = 'checkbox';
+            checkItem.id=writerName;
+            labelItem.htmlFor =writerName;
+            labelItem.textContent=writerName;
+            newDiv.appendChild(checkItem);
+            newDiv.appendChild(labelItem);
+        }
+
 }
--- BDTK/Liste_BD/JS/liste_bd.js
+++ BDTK/Liste_BD/JS/liste_bd.js
@@ -1,23 +1,32 @@
+//ajout de la div receptrice de la création
 let gallery = document.getElementById('bdGallery');
 
+//on utilise la fonction se trouvant: ./data/code_exemplaires.js ligne 20
+let newTab= remplirNewTab(); //fonction permettant de créer un tableau et stocker des elements map qui nous seront utile plus tard
+
+//on effectue l'action au chargement de la page
 window.addEventListener('load',(()=>{showBD(gallery)}));
 
-
+/**
+ * la fonction permet d'afficher la liste des BD
+ * @param {div}  la div dans laquelle ajouter les nouvelle balises 
+ */
 function showBD(div)
 {
-    removeChild(div);
+    removeAllChild(div); //on reinitialise la div
 
-
-    for(let i = 0; i<29; i++)
+    //on créer autant de div qu'on a de BD (30 pour le projet)
+    for(let i = 0; i<30; i++)
     {
-        createBD(div,i)
-        
+        createBD(div,i)   
     }
-
 }
 
-
-function removeChild(div)
+/**
+ * Permet de supprimer tous les enfants d'une div
+ * @param {div} la div dans lesquelle on supprime le div enfants 
+ */
+function removeAllChild(div)
 {
     while(div.firstChild)
     {
@@ -25,13 +34,17 @@ function removeChild(div)
     }
 }
 
+/**
+ * permet de créer un nombre i de div "BD"
+ * @param {div} la div receptrice des nouvelles div 
+ * @param {Int} l'indice actuelle 
+ */
 function createBD(div,indice)
 {
     //initialisation des div à créer
     let newDiv = document.createElement('div');
     let newImg = document.createElement('img');
     let newTitle = document.createElement('h1');
-    let newTab= remplirNewTab();
 
     newDiv.setAttribute('class','bdDiv');
     //valeurs pour la source de l'image
