2a62e1147af08a70b35215e00c3602251c8ec00f
AMMV21 <hatsune2108@gmail.com>
2023-12-12 11:25:12 +0100

    mis a jour

---
+++ BDTK/Liste_BD/JS/liste_bd.js
@@ -0,0 +1,53 @@
+let gallery = document.getElementById('bdGallery');
+
+window.onload(showBD(gallery));
+
+
+function showBD(div)
+{
+    removeChild(div);
+
+
+    for(let i = 0; i<29; i++)
+    {
+        createBD(div,i)
+        
+    }
+
+}
+
+
+function removeChild(div)
+{
+    while(div.firstChild)
+    {
+        div.removeChild(div.lastChild);
+    }
+}
+
+function createBD(div,indice)
+{
+    //initialisation des div à créer
+    let newDiv = document.createElement('div');
+    let newImg = document.createElement('img');
+    let newTitle = document.createElement('h1');
+    let newTab= remplirNewTab();
+
+    //valeurs pour la source de l'image
+    let titleVolume = newTab[indice][1].titre;
+    let numeroVolume = newTab[indice][1].numero;
+    let valSerie = newTab[indice][1].idSerie;
+    let titleSerie = series.get(valSerie).nom;
+
+
+    //initialisation image
+    newImg.src='./Ressources/albumsMini/' +titleSerie+'-'+numeroVolume+'-'+titleVolume+'.jpg';
+
+
+   //on change le nom de la balise titre
+    newTitle.textContent = titleVolume;
+
+    div.appendChild(newDiv);
+    newDiv.appendChild(newImg);
+    newDiv.appendChild(newTitle);
+} 
