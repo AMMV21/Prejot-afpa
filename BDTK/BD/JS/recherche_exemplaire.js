f420335b02165fd7a1238d653943804dae7c3141
Ivann-Elo <138256764+Ivann-Elo@users.noreply.github.com>
2023-12-16 18:52:59 +0100

    Locale storage - OK

--- BDTK/BD/JS/recherche_exemplaire.js
+++ BDTK/BD/JS/recherche_exemplaire.js
@@ -1,123 +1,142 @@
-// Recuperation des elements HTML 
-let btnRechercher = document.getElementById("btnRechercher");
-let codeEx = document.getElementById("codeEx");
-let numAdh = document.getElementById("numAdherant");
-let zoneErrCodeEx = document.getElementById('zoneErrCodeEx');
-let zoneErrNumAdh = document.getElementById('zoneErrNumAdh'); 
+    // Récuperation des éléments HTML 
+    let btnRechercher = document.getElementById("btnRechercher");
+    let btn_pret = document.getElementById("btn_pret");
+    let codeEx = document.getElementById("codeEx");
+    let numAdh = document.getElementById("numAdherant");
+    let zoneErrCodeEx = document.getElementById('zoneErrCodeEx');
+    let zoneErrNumAdh = document.getElementById('zoneErrNumAdh'); 
 
-let zoneCodeExemplaire = document.getElementById('zoneCodeExemplaire');
-let zoneTitre = document.getElementById('zoneTitre');
-let zoneAuteur = document.getElementById('zoneAuteur');
-let zoneSerie = document.getElementById('zoneSerie');
+    let zoneCodeExemplaire = document.getElementById('zoneCodeExemplaire');
+    let zoneTitre = document.getElementById('zoneTitre');
+    let zoneAuteur = document.getElementById('zoneAuteur');
+    let zoneSerie = document.getElementById('zoneSerie');
 
-let zoneNom = document.getElementById('zoneNom');
-let zonePrenom = document.getElementById('zonePrenom');
+    let zoneNom = document.getElementById('zoneNom');
+    let zonePrenom = document.getElementById('zonePrenom');
 
-let validerPret = document.getElementById('validerPret');
-let rechercheInfos = document.getElementById('rechercheInfos');
+    let validerPret = document.getElementById('validerPret');
+    let rechercheInfos = document.getElementById('rechercheInfos');
 
-// Initialisation des variables 
-let codeTrouvee = false ;   
-let numTrouvee = false ; 
+    // Récupere les données dans la mémoire du navigateur
+    let adherentStorage = new Map(JSON.parse(localStorage.getItem("adherent")));
 
-// Expressions régulières  
-let conditionSaisiCodeEx = /^[A-Za-z]\d{3}$/ ; // Ma regexp demande une lettre suivi de 3 chiffres; 
-let conditionSaisiNumAdh = /^\d+$/ ; // Ma regexp demande uniquement 1 ou plusieurs chiffres; 
+    // Initialisation des variables 
+    let codeTrouvee = false ;   
+    let numTrouvee = false ; 
 
-// Abbonements 
-btnRechercher.addEventListener("click",function(){ 
-    
-    zoneCodeExemplaire.innerText = "";
-    zoneTitre.innerText = "";
-    zoneAuteur.innerText = "";
-    zoneSerie.innerText = "";
-
-    zoneErrCodeEx.innerText= "";  // Utile pour réinitialiser à zéro les spans erreur.
-    zoneErrNumAdh.innerText= "";  // Utile pour réinitialiser à zéro les spans erreur.
-
-if(conditionSaisiCodeEx.test(codeEx.value) && conditionSaisiNumAdh.test(numAdh.value)){
-   codeTrouvee = rechercheEx();
-   numTrouvee = rechercheAdh();
- 
-
-            if(codeTrouvee == true && numTrouvee == true){
-            }
-            } else {
-                if(!conditionSaisiCodeEx.test(codeEx.value)){ 
-                    zoneErrCodeEx.innerText = "Commence par une lettre et suivi 3 chiffres";
-                    }
-                if(!conditionSaisiNumAdh.test(numAdh.value)){
-                    zoneErrNumAdh.innerText= "Uniquement des chiffres !" 
-                    } 
-                    
-       }
-});
-
-/**
-//  * Compare le code saisi dans l'input et vérifie ca présence dans le Map exemplaire.
-//  * 
-//  */
-function rechercheEx() {
-    let codeSaisi = codeEx.value;
-    for (let key of Exemplaire.keys()) {
-        if (codeSaisi.toUpperCase() === Exemplaire.get(key).codeExemplaire) {
-            zoneCodeExemplaire.innerText = Exemplaire.get(key).codeExemplaire ;
-            zoneTitre.innerText = Exemplaire.get(key).titre ;
-            zoneAuteur.innerText= Exemplaire.get(key).Auteur ;
-            zoneSerie.innerText = Exemplaire.get(key).Serie;
-            return true; // Déplacez cette ligne ici pour sortir de la fonction après avoir trouvé une correspondance
-        }
-    }
-    // Si la boucle se termine sans trouver de correspondance
-    Swal.fire({
-        icon: "error",
-        title: "Code exemplaire introuvable !",
-        text: "Veuillez saisir le code à nouveau",
-    });  
-    return false;
-}
+    // Expressions régulières  
+    let conditionSaisiCodeEx = /^[A-Za-z]\d{3}$/ ; // Ma regexp demande une lettre suivi de 3 chiffres; 
+    let conditionSaisiNumAdh = /^\d+$/ ; // Ma regexp demande uniquement 1 ou plusieurs chiffres; 
 
 
-/**
- * Compare le numéro saisi dans l'input et vérifie sa présence dans le Map adhérent.
- *  
- */
-function rechercheAdh() {
-    let numAdhSaisi = numAdh.value;
-    numAdhSaisi = (numAdhSaisi < 10) ? "0" + numAdhSaisi : numAdhSaisi;
-    
-    for (let key of adherent.keys()) {
-        if (numAdhSaisi === adherent.get(key).numeroAdherent) {
-           // idRef2.innerText = "Numéro adhérent = " + adherent.get(key).numeroAdherent  + " Nom = " + adherent.get(key).nom + " Prénom = " + adherent.get(key).prenom + " Cotisation = " + adherent.get(key).cotisation;
-            zoneNom.innerText = adherent.get(key).nom;
-            zonePrenom.innerText = adherent.get(key).prenom;
-            
-            if ((adherent.get(key).cotisation === "A jour" || adherent.get(key).emprunt >= 3 ) && codeTrouvee === true){
-                validerPret.classList.remove("invisible");
-            if (adherent.get(key).cotisation === "A jour" && adherent.get(key).emprunt <=3){
-                rechercheInfos.classList.add('invisible');
-            }
-            
-        } else if (codeTrouvee === true){
-            Swal.fire({
-                icon: "error",
-                title: "Prêt impossible !",
-                text: "Consulter le profil adhérant pour régulariser la situation",
-                footer: '<a href="./profilAdherents.html">Consulter le profil</a>'
-            });
-            }
-            return true
-        } 
-    }
-    // Si la boucle se termine sans trouver de correspondance
-    Swal.fire({
-        icon: "error",
-        title: "Ce numéro d'adhérent est introuvable !",
-        text: "Veuillez saisir le code à nouveau",
-      });
-    return false
+    //*************  PROGRAMME PRINCIPALE ************************
+
+   // Abbonements 
+    btnRechercher.addEventListener("click",function(){  
         
+        zoneCodeExemplaire.innerText = "";
+        zoneTitre.innerText = "";
+        zoneAuteur.innerText = "";
+        zoneSerie.innerText = "";
 
-}
+        zoneErrCodeEx.innerText= "";  // Utile pour réinitialiser à zéro les spans erreur.
+        zoneErrNumAdh.innerText= "";  // Utile pour réinitialiser à zéro les spans erreur.
 
+        if(conditionSaisiCodeEx.test(codeEx.value) && conditionSaisiNumAdh.test(numAdh.value)){
+            codeTrouvee = rechercheEx();
+            numTrouvee = rechercheAdh();
+        }
 
+        if(!conditionSaisiCodeEx.test(codeEx.value)){ 
+            zoneErrCodeEx.innerText = "Commence par une lettre et suivi 3 chiffres";
+        }
+
+        if(!conditionSaisiNumAdh.test(numAdh.value)){
+            zoneErrNumAdh.innerText= "Uniquement des chiffres !" 
+        }               
+        
+    });
+   
+    let indice = 0; // Initialisez la variable à zéro
+
+    function rechercheEx() {
+        let codeSaisi = codeEx.value;
+    
+        for (let i=0 ; i < exemplaireStorage.length ; i++) {
+            if (codeSaisi.toUpperCase() === exemplaireStorage[i].codeExemplaire) {
+                zoneCodeExemplaire.innerText = exemplaireStorage[i].codeExemplaire;
+                zoneTitre.innerText = exemplaireStorage[i].titre;
+                zoneAuteur.innerText = exemplaireStorage[i].Auteur;
+                zoneSerie.innerText = exemplaireStorage[i].Serie;
+    
+                indice = i; // Utilisez la clé directement, pas la valeur associée à la clé
+    
+                // Si la BD est indisponible ALERT
+                if (exemplaireStorage[indice].disponibilite === false) {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Exemplaire non disponible!",
+                        text: "L'exemplaire est déjà emprunté, ou vous avez fait une erreur de saisie.",
+                    });
+                    return false;
+                }
+                return true; 
+            }
+        }
+        return false;
+    }
+    
+    // Au click sur le bouton prêt la valeur de disponibilité change 
+    btn_pret.addEventListener('click', function () {
+        alert("clické");
+        if (rechercheEx()) {
+            exemplaireStorage[indice].disponibilite = false;
+            localStorage.setItem("exemplaires", JSON.stringify(exemplaireStorage));
+        }
+    });
+
+   
+    
+    /**
+     * Compare le numéro saisi dans l'input et vérifie sa présence dans le Map adhérent.
+     *  
+     */
+    function rechercheAdh() {
+        let numAdhSaisi = numAdh.value;
+        numAdhSaisi = (numAdhSaisi < 10) ? "0" + numAdhSaisi : numAdhSaisi;
+        
+        for (let key of adherent.keys()) {
+            if (numAdhSaisi === adherent.get(key).numeroAdherent) {
+                zoneNom.innerText = adherent.get(key).nom;
+                zonePrenom.innerText = adherent.get(key).prenom;
+                
+                if ((adherentStorage.get(key).cotisation === "A jour" || adherentStorage.get(key).emprunt >= 3 ) && codeTrouvee === true){
+                    validerPret.classList.remove("invisible");
+                if (adherentStorage.get(key).cotisation === "A jour" && adherentStorage.get(key).emprunt <=3){
+                    rechercheInfos.classList.add('invisible');
+                }
+                
+            } else if (codeTrouvee === true){
+                Swal.fire({
+                    icon: "error",
+                    title: "Prêt impossible !",
+                    text: "Consulter le profil adhérant pour régulariser la situation",
+                    footer: '<a href="./profilAdherents.html">Consulter le profil</a>'
+                });
+                }
+                return true ;
+            } 
+        }
+        // Si la boucle se termine sans trouver de correspondance
+        Swal.fire({
+            icon: "error",
+            title: "Ce numéro d'adhérent est introuvable !",
+            text: "Veuillez saisir le code à nouveau",
+        });
+        return false ;
+    }
+
+  
+
+    // CREATION PAR PETITES FONCTIONS 
+    
--- BDTK/data/code_exemplaires.js
+++ BDTK/data/code_exemplaires.js
@@ -1,58 +1,80 @@
-    // DECLARATION DES VARIABLES 
-        // Les valeurs seront utilisé pour créer un exemplaire
-        var Exemplaire = new Map (); 
-        var  valeurCle = "";
-        var  valAuteur = "";
-        var  valSerie = "";
-        var exemplaireBd = "";
+// DECLARATION DES VARIABLES
+// Les valeurs seront utilisées pour créer un exemplaire
+    var Exemplaire = new Map();
+    let tabLocal = [];
+    var valeurCle = "";
+    var valAuteur = "";
+    var valSerie = "";
 
-    let MapExemplaire = creerMapExemplaires();
 
-    // *********** MES FONCTIONS **************
-    
+//                          *****************    MES FONCTIONS     **************
     /**
-     * Le programme me permet de créer un code exemplaire à chaque boucle et ainsi de créer et récuperer le titre, nom, série
-     * @returns chaine de caractères
+     * La fonction crée 3 exemplaires de chaque BD et leur assigne un CODE EXEMPLAIRE unique
+     * et une disponibilité true ou false
+     * @returns Map
     */
-   function creerMapExemplaires(){   
+    function creerMapExemplaires() {
         let k = 0;
         let codeExemplaire = 0;
-        let cle = 1;
+        let cle = 0;
 
-        for (let i=0 ; i<=29 ;i++){
-         let nbrExemplaire = Math.floor(Math.random() * 5) + 1;
- 
-             for ( let j=1 ; j<= nbrExemplaire ; j++) {
- 
-                 valeurCle = tliste_bd[i][0]; // NUMERO DE LA CLE ( reste à ajouter le titre)
-                 valAuteur = tliste_bd[i][1].idAuteur; // ID DE L'AUTEUR (reste à ajouter le nom)
-                 valSerie = tliste_bd[i][1].idSerie; // ID DE LA SERIE (reste à ajouter le nom)
-                 
-                 codeExemplaire = (k < 10) ? "A00" + k : "A0" + k;
-                 // Création d'un exemplaire avec l'attribution de son code
-                 exemplaireBd = Exemplaire.set(cle,{codeExemplaire: codeExemplaire , titre: albums.get(valeurCle).titre, Auteur: auteurs.get(valAuteur).nom, Serie: series.get(valSerie).nom});
-                 k ++;
-                 cle ++;
-                 console.log("valeur de i = " + i);
-                 console.log("valeur de j = " +j);
-                 console.log("Valeur de K = " + k);
-                 console.log("Nombre d'exemplaire = " + nbrExemplaire);
+        for (let i = 0; i <= 29; i++) {
+            for (let j = 1; j <= 3; j++) {
+                valeurCle = tliste_bd[i][0]; // NUMERO DE LA CLE (reste à ajouter le titre)
+                valAuteur = tliste_bd[i][1].idAuteur; // ID DE L'AUTEUR (reste à ajouter le nom)
+                valSerie = tliste_bd[i][1].idSerie; // ID DE LA SERIE (reste à ajouter le nom)
+
+                codeExemplaire = (k < 10) ? "A00" + k : "A0" + k;
+                // Création d'un exemplaire avec l'attribution de son code
+                // Initialise la disponibilité à disponible si k est impair
+                if (k % 2 !== 0) {
+                    Exemplaire.set(cle, {
+                        codeExemplaire: codeExemplaire,
+                        titre: albums.get(valeurCle).titre,
+                        Auteur: auteurs.get(valAuteur).nom,
+                        Serie: series.get(valSerie).nom,
+                        Emprunteur: "Numéro adhérent : " + k + " ",
+                        disponibilite: true ,
+                    });
+                } else {
+                    // Initialise la disponibilité à disponible si k est pair
+                    Exemplaire.set(cle, {
+                        codeExemplaire: codeExemplaire,
+                        titre: albums.get(valeurCle).titre,
+                        Auteur: auteurs.get(valAuteur).nom,
+                        Serie: series.get(valSerie).nom,
+                        Emprunteur: "",
+                        disponibilite: false
+                    });
                 }
-            console.log("Fin de répétition création code exemplaire");
+                k++;
+                cle++;
+            }
+        }
+    }   
  
-         }  return exemplaireBd;
-     }
- 
-     console.log(exemplaireBd)
+   // A partir de ma map je rempli un nouveau tableau qui contient des uniquement les objet 
+   // cela facilite la l'accés au données du tableau
+   const remplirNew = function () {
 
-//console.log(Exemplaire.get("1").Serie);
-//console.log(Exemplaire.get("1").codeExemplaire) // Je controle est affiche un code exemplaire
-// console.log(Exemplaire.get("1").titre) // Je controle est affiche le nom
-// console.log(Exemplaire.get("1").Auteur) // Je controle est affiche l'auteur
-
-// console.log(Exemplaire);
-// console.log(Exemplaire.get("6").codeExemplaire) // Je controle est affiche un code exemplaire
-// console.log(Exemplaire.get("6").titre) // Je controle est affiche le nom
-// console.log(Exemplaire.get("6").Auteur) // Je controle est affiche l'auteur
+    Exemplaire.forEach(element => {  // Parcouri chaque élément du tableau "Exemplaire".
+      if (typeof element === 'object' && element !== null) { // Vérifier si l'élément est un objet et non null.
+            tabLocal.push(element); // ajout de l'élément au tableau 
+      }
+    });
+    return tabLocal; 
+   };
 
 
+//                         ******************* PROGRAMME PRINCIPALE **************
+creerMapExemplaires(); 
+let exemplaireStorage = remplirNew();   
+console.log(exemplaireStorage)
+
+// *******************  Vérifie si le locale storage à déja été créée dans la session 
+if (!exemplaireStorage){ localStorage.setItem("exemplaires", JSON.stringify(Array.from(exemplaireStorage))); } 
+
+// Je décommente la ligne suivant pour réinitialiser le storage avec le tableau de base.
+//localStorage.setItem("exemplaires", JSON.stringify(Array.from(exemplaireStorage))); 
+
+   
