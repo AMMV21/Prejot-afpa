6b0b70ac217fd936c4b0a53bec954c6a34838620
AMMV <135138122+AMMV21@users.noreply.github.com>
2023-12-19 13:13:26 +0100

    Update liste_bd.js

--- BDTK/Liste_BD/JS/liste_bd.js
+++ BDTK/Liste_BD/JS/liste_bd.js
@@ -92,8 +92,8 @@ function createBD(div,indice,title,exact)
             else if(checkTitleFilter(div,titleVolume,title))
             {
                 //initialisation image
-                newImg.src=`./Ressources/albums/${titleSerie}-${numeroVolume}-${titleVolume}.jpg`;
-                newImg.alt=`${titleVolume}_cover`;
+                newImg.src='./Ressources/albums/' +titleSerie+'-'+numeroVolume+'-'+titleVolume+'.jpg';
+                newImg.alt=`'couverture '+ titleVolume;
                 
                 //on change le nom de la balise titre
                 newTitle.textContent = titleVolume;
