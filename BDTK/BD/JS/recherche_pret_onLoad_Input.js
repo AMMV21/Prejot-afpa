d96ab145431064aabdc67c1edb3569e98fc0ad08
AMMV <135138122+AMMV21@users.noreply.github.com>
2023-12-20 10:52:08 +0100

    Update recherche_pret_onLoad_Input.js

--- BDTK/BD/JS/recherche_pret_onLoad_Input.js
+++ BDTK/BD/JS/recherche_pret_onLoad_Input.js
@@ -1,10 +1,14 @@
 window.addEventListener('load',()=>{checkCopyUrl()});
 
+/*
+*Permet de rechercher une information "numeroId" dans l'url
+*/
 function checkCopyUrl()
 {
    let url = window.location.href;
    let searchParams = new URLSearchParams(url);
 
+   //si l'url comporte une donnée "numeId" alors l'input "codeEx" prend la valeur de la donnée
    if(searchParams.has('numeroId'))
    {
         codeEx.value = searchParams.get('numeroId');
